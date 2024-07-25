88a1770f87a5237b7d05083037320c76dba93bdf
Igor Zanella <120991541+ZanellaIgor@users.noreply.github.com>
2024-07-24 23:23:41 -0300

    implementado rotas backend

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { env } from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+  console.log(`Listening on port ${env.PORT}`);
+  await app.listen(env.PORT || 3001);
 }
 bootstrap();
